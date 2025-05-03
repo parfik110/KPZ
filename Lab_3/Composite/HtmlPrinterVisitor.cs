cd6fd4709ccaf25d77b9b598e73d14441ab79962
Parfeniyk Oleksandr <ipz235_pog@student.ztu.edu.ua>
2025-05-03 17:17:49 +0300

    Add Visitor pattern with full support for all node types

---
+++ Lab_3/Composite/HtmlPrinterVisitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class HtmlPrinterVisitor : INodeVisitor
+    {
+        public void VisitElement(LightElementNode element)
+        {
+            Console.WriteLine($"[Element] <{element.TagName}>");
+        }
+
+        public void VisitText(LightTextNode text)
+        {
+            Console.WriteLine($"[Text] {text.InnerHTML}");
+        }
+
+        public void VisitImage(LightImageNode image)
+        {
+            Console.WriteLine($"[Image] src=\"{image.Source}\" alt=\"{image.Strategy.Load(image.Source)}\"");
+        }
+    }
+}
